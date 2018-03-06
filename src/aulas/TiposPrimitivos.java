011fa4c9abf7a35de812fe058a3aa1e7cf15362a
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-03-06 07:55:01 -0300

    Tipos primitivos Upload

---
+++ src/aulas/TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package aulas;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		//Variáveis de Tipo Primitivo
+		char c = 'a';
+		int i = 10;
+		short s = 15;
+		long l = 20;
+		float f = 25.0f;
+		double d = 30.0;
+	}
+}
